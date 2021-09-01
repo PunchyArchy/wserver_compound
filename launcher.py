b48e0e9714656a5d9d7ac6962826f354c7542d7f
PunchyArchy <ksmdrmvstchny@gmail.com>
2021-09-01 10:57:15 +0500

    Добавлен launcher

--- launcher.py
+++ launcher.py
@@ -1,3 +1,15 @@
-from wserver_compound.main import WServer
+""" Запустить WServer, извлекая из переменной окружения данные для доступа
+к GDB."""
 
-inst = WServer(8888)
+
+from wserver_compound.main import WServer
+import os
+
+dbname = os.environ.get('GDBNAME')
+dbuser = os.environ.get('GDBUSER')
+dbpass = os.environ.get('GDBPASS')
+dbhost = os.environ.get('GDBHOST')
+
+inst = WServer(8888, dbname=dbname, dbuser=dbuser, dbpass=dbpass,
+               dbhost=dbhost)
+inst.start()
