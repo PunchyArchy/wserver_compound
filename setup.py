3bfce1bf77c446422ca11df401c80916ab3206f6
Евген <evgeny.kozhevatov@yandex.ru>
2021-10-22 16:53:44 +0300

    le_pod

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 from os.path import join, dirname
 
 setup(
-    name='wserver_compound',
-    version='0.0.2',
+    name='wserver_compound_beta',
+    version='0.0.1',
     packages=find_packages(),
-    author='PunchyArchy',
-    author_email='ksmdrmvscthny@gmail.com',
-    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    author='Plyoushkin',
+    author_email='plyoushkin.evgen@gmail.com',
+    #long_description=open(join(dirname(__file__), 'README.txt')).read(),
     install_requires=[
         'qodex_pi',
         'wsqluse',
